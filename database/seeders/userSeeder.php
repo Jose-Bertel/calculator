13d2e2246012080e1d92dd7caf66d81d02e31988
Jose Alberto Bertel Martinez <jose.bertelm@cecar.edu.co>
2025-06-03 09:40:54 -0500

    Se agrega el proyecto

---
+++ database/seeders/userSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Faker\Factory as Faker;
+
+class userSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        $faker = Faker::create();
+        // Administrador
+        $admin = User::create(
+            [   'email' => 'josebertel2004@gmail.com',
+                'email_verified_at' => now(),
+                'name' => 'Jose Alberto Bertel Martinez',
+                'password' => Hash::make('Admin123!'),
+            ]
+        );
+        $admin->assignRole('ADMINISTRADOR');
+
+        // $estudiante = User::create(
+        //     [   'email' => $faker->unique()->safeEmail(),
+        //         'email_verified_at' => now(),
+        //         'name' => $faker->name(),
+        //         'password' => Hash::make('provicional'),
+        //     ]
+        // );
+        // $estudiante->assignRole('USUARIO');
+    }
+}
